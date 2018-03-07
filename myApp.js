580bd29f15e030023ab13a59e1385e8c5a9d9b90
Syngc <cynthiagviria@gmail.com>
2018-03-07 01:08:37 -0500

    second commit

--- myApp.js
+++ myApp.js
@@ -19,7 +19,11 @@ myApp.get('/persona/:id', function (req, res) {
 });
 
 myApp.post('/persona', (req, res) => {
-  per = req.body;
+  bod = req.body;
+  per = new persona();
+  per.id = bod.id;
+  per.nombre =  bod.nombre;
+  per.edad = bod.edad;
   personas.push(per);
   res.status(200).send({message: 'La persona ha sido agregada'});
 });
@@ -29,10 +33,11 @@ myApp.put('/Persona/:id', (req, res) =>{
   if(found == -1){
     res.status(404).send({message:'La persona no existe'});
   }
-  personas[found].name = req.body.name;
-  personas[found].edad = req.body.edad;
+  personas[found].nombre = valor(req.body.nombre, personas[found].nombre);
+  personas[found].edad = valor(req.body.edad, personas[found].edad);
   res.status(200).send({message: 'Los datos han sido actualizados'})
 });
+
 
 myApp.delete('/Persona/:id', (req, res) =>{
   var found = personas.findIndex( x => x.id == req.params.id);
@@ -43,25 +48,117 @@ myApp.delete('/Persona/:id', (req, res) =>{
   res.status(200).send({message: 'La persona ha sido eliminada'})
 });
 
-
-/*
 //Estudiante
-myApp.get('/Estudiante', (req,res) => res.send('Persona'))
-myApp.post('/', (req, res) => res.send(''))
-myApp.put()
-myApp.delete()
+myApp.get('/estudiante', function (req, res) {
+  res.status(200).send({Estudiantes: [estudiantes]});
+});
+
+myApp.get('/estudiante/:id', function (req, res) {
+  var found = estudiantes.findIndex( x => x.id == req.params.id);
+  if(found == -1){
+    res.status(404).send({message:'El estudiante no existe'});
+  }
+  res.status(200).send({Estudiante: [estudiantes[found]]});
+});
+
+myApp.post('/estudiante', (req, res) => {
+  bod = req.body;
+  per = new estudiante();
+  per.id = bod.id;
+  per.nombre =  bod.nombre;
+  per.edad = bod.edad;
+  per.carrera = bod.carrera;
+  per.semestre = bod.semestre;
+  estudiantes.push(per);
+  res.status(200).send({message: 'El estudiante ha sido agregado'});
+});
+
+myApp.put('/estudiante/:id', (req, res) =>{
+  var found = estudiantes.findIndex( x => x.id == req.params.id);
+  if(found == -1){
+    res.status(404).send({message:'El estudiante no existe'});
+  }
+  estudiantes[found].nombre = valor(req.body.nombre, estudiantes[found].nombre);
+  estudiantes[found].edad = valor(req.body.edad, estudiantes[found].edad);
+  estudiantes[found].semestre = valor(req.body.semestre, estudiantes[found].semestre);
+  estudiantes[found].carrera = valor(req.body.carrera, estudiantes[found].carrera);
+  res.status(200).send({message: 'Los datos han sido actualizados'})
+});
+
+myApp.delete('/estudiante/:id', (req, res) =>{
+  var found = estudiantes.findIndex( x => x.id == req.params.id);
+  if(found == -1){
+    res.status(404).send({message:'El estudiante no existe'});
+  }
+  estudiantes.splice(found, 1);
+  res.status(200).send({message: 'El estudiante ha sido eliminado'})
+});
 
 //Profesor
-myApp.get('/Profesor', (req,res) => res.send('Persona'))
-myApp.post('/', (req, res) => res.send(''))
-myApp.put()
-myApp.delete()
-*/
+myApp.get('/profesor', function (req, res) {
+  res.status(200).send({Profesores: [profesores]});
+});
+
+myApp.get('/profesor/:id', function (req, res) {
+  var found = profesores.findIndex( x => x.id == req.params.id);
+  if(found == -1){
+    res.status(404).send({message:'El profesor no existe'});
+  }
+  res.status(200).send({Profesor: [profesores[found]]});
+});
+
+myApp.post('/profesor', (req, res) => {
+  bod = req.body;
+  per = new profesor();
+  per.id = bod.id;
+  per.nombre =  bod.nombre;
+  per.edad = bod.edad;
+  per.contrato = bod.contrato;
+  per.materia = bod.materia;
+  profesores.push(per);
+  res.status(200).send({message: 'El profesor ha sido agregado'});
+});
+
+myApp.put('/profesor/:id', (req, res) =>{
+  var found = profesores.findIndex( x => x.id == req.params.id);
+  if(found == -1){
+    res.status(404).send({message:'El profesor no existe'});
+  }
+  profesores[found].nombre = valor(req.body.nombre, profesores[found].nombre);
+  profesores[found].edad = valor(req.body.edad, profesores[found].edad);
+  profesores[found].contrato = valor(req.body.contrato, profesores[found].contrato);
+  profesores[found].materia = valor(req.body.materia, profesores[found].materia);
+  res.status(200).send({message: 'Los datos han sido actualizados'})
+});
+
+myApp.delete('/profesor/:id', (req, res) =>{
+  var found = profesores.findIndex( x => x.id == req.params.id);
+  if(found == -1){
+    res.status(404).send({message:'El profesor no existe'});
+  }
+  profesores.splice(found, 1);
+  res.status(200).send({message: 'El profesor ha sido eliminado'})
+});
+
 myApp.listen(3001, () => console.log('Escuchando por el puerto 3001'));
 
 //Objetos
-//var persona = {  $id: '12345',  $nombre: 'Mateo Cano', $edad: ' 21 ',};
-personas = [];
+
+personas = [
+  {id:'9753' , nombre:'Mia', edad:'43'},
+  {id:'8642' , nombre:'Moises',  edad:'42'},
+  {id:'7413' , nombre:'Sandra', edad:'87'}
+];
+estudiantes = [
+  {id:'1234' , nombre:'Luis', edad:'21', carrera:'Psicologia', semestre:'2'},
+  {id:'5678' , nombre:'Ana',  edad:'24', carrera:'Derecho', semestre:'7'},
+  {id:'9012' , nombre:'Miguel', edad:'27', carrera:'Ingenieria', semestre:'1'}
+];
+profesores = [
+  {id:'9876' , nombre:'Javier', edad:'19',  materia:'Algebra', contrato:'Catedra' },
+  {id:'7654' , nombre:'Ismael',  edad:'29',  materia:'Calculo', contrato:'Planta'},
+  {id:'4321' , nombre:'Luna', edad:'40', materia:'Fisica', contrato:'Catedra' }
+];
 
 function persona(id, nombre, edad){
   this.id = id;
@@ -69,5 +166,19 @@ function persona(id, nombre, edad){
   this.edad = edad;
 }
 
-var Estudiante = Object.create(persona);
-var Profesor = Object.create(persona);
+function estudiante(id, nombre, edad, carrera, semestre){
+  this.carrera = carrera;
+  this.semestre = semestre;
+}
+estudiante.prototype = new persona;
+
+function profesor(id, nombre, edad, materia, contrato){
+  this.materia = materia;
+  this.contrato = contrato;
+}
+
+function valor(x, y){
+  return Boolean(x) ? x : y;
+}
+
+profesor.prototype = new persona;
